4ac3730e969cff7409b4a24d2edd3941e3b8a6a6
Arush <arush.yadlapati@gmail.com>
2025-04-10 17:58:52 -0700

    Add local copy of core + other setup structure

---
+++ TeamCode/src/main/java/com/seattlesolvers/solverslib/util/MathUtils.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.seattlesolvers.solverslib.util;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public final class MathUtils {
+    private MathUtils() {
+        throw new AssertionError("utility class");
+    }
+
+    /**
+     * Returns value clamped between low and high boundaries.
+     *
+     * @param value Value to clamp.
+     * @param low   The lower boundary to which to clamp value.
+     * @param high  The higher boundary to which to clamp value.
+     */
+    public static int clamp(int value, int low, int high) {
+        return Math.max(low, Math.min(value, high));
+    }
+
+    /**
+     * Returns value clamped between low and high boundaries.
+     *
+     * @param value Value to clamp.
+     * @param low   The lower boundary to which to clamp value.
+     * @param high  The higher boundary to which to clamp value.
+     */
+    public static double clamp(double value, double low, double high) {
+        return Math.max(low, Math.min(value, high));
+    }
+
+    /**
+     * Returns value rounded to specified places
+     *
+     * @param number Number to round
+     * @param places The number of decimal places to round to
+     */
+
+    public static double round(double number, int places) {
+        return new BigDecimal((String.valueOf(number))).setScale(places, RoundingMode.HALF_UP).doubleValue();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp/SwerveOpMode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.opmode.TeleOp;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+
+@Config
+@TeleOp
+public class SwerveOpMode extends CommandOpMode {
+
+    @Override
+    public void initialize() {
+
+    }
+}
